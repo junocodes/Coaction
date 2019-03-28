3be524757220f174a1f545354f1af5bd26df1a83
Justin Juno <hello@juno.codes>
2019-03-28 16:01:56 -0500

    create teams

--- src/components/presentational/teams.js
+++ src/components/presentational/teams.js
@@ -3,10 +3,39 @@ import React, { Component } from "react";
 export default class Teams extends Component {
   constructor(props) {
     super(props);
-    this.state = { teams: [] };
+    this.state = { teams: [], isCreatingTeam: false, value: "" };
 
     // Creating reference for FirebaseDB data.
     this.teamsRef = this.props.firebaseDB.database().ref("teams");
+
+    // Binding form values for team creation.
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleCreatingTeamToggle() {
+    // Toggling visibility for creating team form.
+    this.setState({ isCreatingTeam: !this.state.isCreatingTeam });
+  }
+
+  handleChange(e) {
+    this.setState({ value: e.target.value });
+
+    // Verify state is updating in console
+    console.log(this.state.value);
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    // Define and push new team to FirebaseDB.
+    let newTeam = this.state.value;
+    this.teamsRef.push({
+      name: newTeam
+    });
+
+    // Clearing form input and toggle back to original view.
+    this.setState({ value: "" });
+    this.handleCreatingTeamToggle();
   }
 
   componentDidMount() {
@@ -19,19 +48,43 @@ export default class Teams extends Component {
       team.key = snapshot.key;
       this.setState({ teams: teams.concat(team) });
 
-      // Reference returned FirebaseDB data as table.
+      // Reference returned FirebaseDB teams as table.
       console.table(teams);
     });
   }
 
   render() {
-    const { teams } = this.state;
+    const { teams, isCreatingTeam } = this.state;
     return (
-      <ul className="teams">
-        {teams.map(team => {
-          return <li key={team.key}>{team.name}</li>;
-        })}
-      </ul>
+      <>
+        <ul className="teams">
+          {teams.map(team => {
+            return <li key={team.key}>{team.name}</li>;
+          })}
+        </ul>
+        <div className="create-team">
+          {isCreatingTeam ? (
+            <>
+              <h3>Add Your Team</h3>
+              <form onSubmit={this.handleSubmit}>
+                <input
+                  type="text"
+                  value={this.state.value}
+                  onChange={this.handleChange}
+                />
+                <button>Submit</button>
+              </form>
+              <small onClick={() => this.handleCreatingTeamToggle()}>
+                Cancel
+              </small>
+            </>
+          ) : (
+            <button onClick={() => this.handleCreatingTeamToggle()}>
+              Create Team
+            </button>
+          )}
+        </div>
+      </>
     );
   }
 }
